6116131c4d7a16e0aed0683973ef23dde5cd0b09
christianzakhour <christian.t.z@hotmail.com>
2022-06-15 10:55:01 +0300

    online&offline mode

--- lib/home_page/ui/screens/main_screen.dart
+++ lib/home_page/ui/screens/main_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_offline/flutter_offline.dart';
 import 'package:hooka/buddies/ui/screens/buddies.dart';
+import 'package:hooka/test.dart';
+import 'package:hooka/utils/style/colors.dart';
 import '../../../hooka_places/ui/screen/hooka_places.dart';
 import '../../../hooka_product/ui/screen/hooka_product.dart';
 import 'package:hooka/home_page/ui/widget/hooka_card.dart';
@@ -20,10 +23,9 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
           elevation: 1,
-
           title: Text(
             "Hookapp",
             style: TextStyle(color: Colors.black),
@@ -33,7 +35,7 @@ class _MainScreenState extends State<MainScreen> {
           actions: [
             IconButton(
                 onPressed: () {
-
+                  Navigator.push(context, CustomPageRoute(child: DemoPage()));
                 },
                 icon: Icon(
                   Icons.notifications,
@@ -45,50 +47,88 @@ class _MainScreenState extends State<MainScreen> {
                   Icons.shopping_cart,
                   color: Colors.black,
                 )),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
+            OfflineBuilder(
+              connectivityBuilder: (
+                BuildContext context,
+                ConnectivityResult connectivity,
+                Widget child,
+              ) {
+                final bool connected = connectivity != ConnectivityResult.none;
+                return new Stack(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Container(
+                        width: 50,
+                        height: 20,
+                        color: Colors.transparent,
+                        child: Center(
+                          child:
+                          connected ?
+                              Text('Online',style: TextStyle(color: YellowColor,fontWeight: FontWeight.bold,decoration: TextDecoration.underline),):
+                              Text('Offline',style: TextStyle(color:  Color(0xFFEE4400),fontWeight: FontWeight.bold,decoration: TextDecoration.lineThrough)),),
+                        ),
+                      ),
+
+                  ],
+                );
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  new Text(
+                    'There are no bottons to push :)',
+                  ),
+                  new Text(
+                    'Just turn off your internet.',
+                  ),
+                ],
+              ),
             ),
-            Image.asset(
-              ImageAsset.LOGO,
-              height: 250,
-              width: 500,
-              fit: BoxFit.cover,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              HookaCard(
-                  image: ImageAsset.PLACES,
-                  onCardTap: () {
-                    Navigator.push(
-                        context, CustomPageRoute(child: HookaPlaces()));
-                  },
-                  text: "HOOKA PLACES"),
-              HookaCard(
-                  image: ImageAsset.BUDDIES,
-                  onCardTap: () {
-                    Navigator.push(
-                        context, CustomPageRoute(child: Buddies()));
-                  },
-                  text: "HOOKA BUDDIES"),
-            ]),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              HookaCard(
-                  image: ImageAsset.OFFER, onCardTap: () {
-                Navigator.push(
-                    context, CustomPageRoute(child: Offers()));
-              }, text: "OFFERS"),
-              HookaCard(
-                  image: ImageAsset.PRODUCT,
-                  onCardTap: () {
-                    Navigator.push(
-                        context, CustomPageRoute(child: HookaProduct()));
-                  },
-                  text: "Hooka product"),
-            ]),
           ]),
-        ));
+      body: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.1,
+          ),
+          Image.asset(
+            ImageAsset.LOGO,
+            height: 250,
+            width: 500,
+            fit: BoxFit.cover,
+          ),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            HookaCard(
+                image: ImageAsset.PLACES,
+                onCardTap: () {
+                  Navigator.push(
+                      context, CustomPageRoute(child: HookaPlaces()));
+                },
+                text: "HOOKA PLACES"),
+            HookaCard(
+                image: ImageAsset.BUDDIES,
+                onCardTap: () {
+                  Navigator.push(context, CustomPageRoute(child: Buddies()));
+                },
+                text: "HOOKA BUDDIES"),
+          ]),
+          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            HookaCard(
+                image: ImageAsset.OFFER,
+                onCardTap: () {
+                  Navigator.push(context, CustomPageRoute(child: Offers()));
+                },
+                text: "OFFERS"),
+            HookaCard(
+                image: ImageAsset.PRODUCT,
+                onCardTap: () {
+                  Navigator.push(
+                      context, CustomPageRoute(child: HookaProduct()));
+                },
+                text: "Hooka product"),
+          ]),
+        ]),
+      ),
+    );
   }
 }
