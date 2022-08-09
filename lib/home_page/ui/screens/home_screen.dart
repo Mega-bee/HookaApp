35d54c30d32ce0d190c8a33607e2bea9c39182d8
christianzakhour <christian.t.z@hotmail.com>
2022-08-09 19:55:30 +0300

    Chris Invitation

--- lib/home_page/ui/screens/home_screen.dart
+++ lib/home_page/ui/screens/home_screen.dart
@@ -131,6 +131,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 text: "Hooka product"),
           ]),
           SizedBox(
+
             height: MediaQuery.of(context).size.height * 0.2,
           ),
         ]),
